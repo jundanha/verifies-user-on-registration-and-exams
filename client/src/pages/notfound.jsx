b05c7f086f150a4dbd71d5f71ae2c39050fa3894
Jundan Haris <jundanha@gmail.com>
2023-12-06 12:47:52 +0700

    update 404 page

--- client/src/pages/notfound.jsx
+++ client/src/pages/notfound.jsx
@@ -1,16 +1,45 @@
-import { Box } from "@chakra-ui/react"
+import { Box, Heading, Link, Text } from "@chakra-ui/react"
+import { Link as RouterLink } from 'react-router-dom';
 
 function NotFoundPage () {
   return (
     <Box
+      bgGradient="linear(to-tr, #B6FFFA, #687EFF)"
+      w="100%" h="100vh"
+      p={4}
+      color="white"
       display="flex"
       alignItems="center"
       justifyContent="center"
-      h="100vh"
-      w='100vw'
+      flexDir="column"
     >
-      <Box w="md" p={8} borderWidth={1} borderRadius={8} boxShadow="lg">
-        <h1>404 - Not Found</h1>
+      <Box
+        display='flex'
+        flexDir='column'
+      >
+        <Heading
+          mb={4}
+          fontSize="6xl"
+          fontWeight="bold"
+          color="white"
+          fontFamily={"sans-serif"}
+          fontStyle={"bold"}
+        >
+          404 - Not Found
+        </Heading>
+        <Text
+        >
+          Sorry, the page you are looking for does not exist.
+        </Text>
+        <Link
+          as={RouterLink}
+          to="/"
+          mt='5'
+          _hover={{ color: 'blue.800' }}
+          textDecoration={"underline"}
+        >
+          Back to Homepage
+        </Link>
       </Box>
     </Box>
   )
