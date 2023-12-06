b95a28fe08c4fa8521cbc7136fcbe4fff505c68e
waksun0x00 <koko41701@gmail.com>
2023-12-06 10:07:11 +0700

    commit gesture track

--- ML/gesture/app.py
+++ ML/gesture/app.py
@@ -1,36 +1,41 @@
 from flask import Flask, render_template, Response
 import cv2
 import tensorflow as tf
-import numpy as np
 
 app = Flask(__name__)
 
-model = tf.keras.models.load_model('model.h5')
+model = tf.keras.models.load_model('./model.h5')
 cap = cv2.VideoCapture(0)
 
+# Modify the `generate_frame` function
 def generate_frame():
     while True:
         status, frame = cap.read()
 
         # Preprocess the frame
         frame = cv2.resize(frame, (250, 250))
-        frame = frame.reshape(1, 250, 250, 3) / 255.0
+        normalize_frame = frame.reshape(1, 250, 250, 3) / 255.0
 
         # Make predictions using the model
-        eye_gaze = model.predict(frame)
+        eye_gaze = model.predict(normalize_frame)
+
+        # Convert the predictions to a string for display
+        eye_gaze_str = str(eye_gaze[0][0])
+
+        # Encode the frame as JPEG
+        _, jpeg_frame = cv2.imencode('.jpg', frame)
+        frame_bytes = jpeg_frame.tobytes()
 
         # Yield the frame and predictions
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + cv2.imencode('.jpg', frame)[1].tobytes() + b'\r\n\r\n' +
-               b'Eye Gaze Predictions: ' + str(eye_gaze) + b'\r\n\r\n')
+        yield f"data:image/jpeg;base64,{frame_bytes.decode('utf-8')}\r\n\r\nEye Gaze Predictions: {eye_gaze_str}\r\n\r\n"
 
 @app.route('/')
 def index():
-    return render_template('index.html')  # You can create an HTML template to display the video feed and predictions
-
+    return render_template('index.html')
+    
 @app.route('/video_feed')
 def video_feed():
-    return Response(generate_frame(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(generate_frame(), mimetype= 'multipart/x-mixed-replace',content_type='text/event-stream')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=3000, debug=True)
