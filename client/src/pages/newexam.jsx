120aed48e6294bff8babc710e596935384df8474
Jundan Haris <jundanha@gmail.com>
2023-12-07 12:55:36 +0700

    Refactor newexam.jsx component

--- client/src/pages/newexam.jsx
+++ client/src/pages/newexam.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useRef } from 'react';
-import { useClipboard, ChakraProvider, Center, VStack, Button, Box, useDisclosure } from '@chakra-ui/react';
+import { useEffect, useRef, useState } from 'react';
+import { useClipboard, Button, Box, useDisclosure, Heading, Select } from '@chakra-ui/react';
 import { Link as RouterLink } from 'react-router-dom';
 import Webcam from 'react-webcam';
 import {
@@ -10,77 +10,223 @@ import {
   ModalFooter,
   ModalBody,
   ModalCloseButton,
-} from '@chakra-ui/react'
+} from '@chakra-ui/react';
 
 function NewExamPage() {
-    const videoRef = useRef();
+  const videoRef = useRef();
+  const webcamRef = useRef(null);
 
-    useEffect(() => {
-      const enableCamera = async () => {
-        try {
-          const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-          if (videoRef.current) {
-            videoRef.current.srcObject = stream;
-          }
-        } catch (error) {
-          console.error('Error accessing camera:', error);
+  const [capturedImage, setCapturedImage] = useState(null);
+  const [cameras, setCameras] = useState([]);
+  const [selectedCamera, setSelectedCamera] = useState('');
+  const [isCaptureDisabled, setCaptureDisabled] = useState(false);
+  const [isRegisterDisabled, setRegisterDisabled] = useState(true);
+
+  useEffect(() => {
+    const enableCamera = async () => {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        if (videoRef.current) {
+          videoRef.current.srcObject = stream;
         }
-      };
-  
-      enableCamera();
-  
-      return () => {
-        if (videoRef.current && videoRef.current.srcObject) {
-          const tracks = videoRef.current.srcObject.getTracks();
-          tracks.forEach(track => track.stop());
-        }
-      };
-    }, []);
-  
-    const { isOpen, onOpen, onClose } = useDisclosure()
-    const { onCopy, value, setValue, hasCopied } = useClipboard("");
-  
-  
-    return (
-      <ChakraProvider>
-        <Box position="absolute" top="10px" left="10px">
-            <Button 
-                variant='outline'
-                as={RouterLink}
-                to="/">Back</Button>
+      } catch (error) {
+        console.error('Error accessing camera:', error);
+      }
+    };
+
+    enableCamera();
+
+    return () => {
+      if (videoRef.current && videoRef.current.srcObject) {
+        const tracks = videoRef.current.srcObject.getTracks();
+        tracks.forEach(track => track.stop());
+      }
+    };
+  }, []);
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { onCopy, value, setValue, hasCopied } = useClipboard("");
+
+  const getAvailableCameras = async () => {
+    try {
+      const devices = await navigator.mediaDevices.enumerateDevices();
+      const videoDevices = devices.filter((device) => device.kind === 'videoinput');
+      setCameras(videoDevices);
+      if (videoDevices.length > 0) {
+        setSelectedCamera(videoDevices[0].deviceId);
+      }
+    } catch (error) {
+      console.error('Error fetching cameras:', error);
+    }
+  };
+
+  const handleCameraChange = (event) => {
+    setSelectedCamera(event.target.value);
+  };
+
+  const videoConstraints = {
+    deviceId: selectedCamera !== '' ? { exact: selectedCamera } : undefined,
+  };
+
+  const captureImage = () => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setCapturedImage(imageSrc);
+    setCaptureDisabled(true);
+    setRegisterDisabled(false);
+    console.log(imageSrc);
+  };
+
+  const registerExam = () => {
+    // Implement registration logic here
+    console.log('Exam registered!');
+  };
+
+  useEffect(() => {
+    getAvailableCameras();
+  }, []);
+
+  return (
+    <>
+      <Box
+        bgGradient="linear(to-tr, #B6FFFA, #687EFF)"
+        w="100%" h="100vh"
+        p={3}
+        color="white"
+        display="flex"
+        alignItems="center"
+        flexDir="column"
+      >
+        <Box
+          display='flex'
+          flexDir='row'
+          alignItems={"center"}
+          justifyContent='space-between'
+          w='100%'
+          px='5'
+        >
+          <Button
+            variant='outline'
+            as={RouterLink}
+            to="/"
+          >
+            Back
+          </Button>
+          <Heading
+            my={4}
+            fontSize="4xl"
+            color="white"
+            fontFamily={"sans-serif"}
+          >
+            Generate New Exam
+          </Heading>
         </Box>
-        <Center h="100vh">
-          <VStack>
+        <Box
+          w='100%'
+          display='flex'
+          flexDir='column'
+          alignItems='center'
+          justifyContent='center'
+        >
+          <Box position="relative" mb={4}>
             <Webcam
               audio={false}
-              ref={videoRef}
-              style={{ width: '100%', height: 'auto' }}
+              height={400}
+              ref={webcamRef}
+              screenshotFormat="image/jpeg"
+              width={600}
+              videoConstraints={videoConstraints}
+              style={{ borderRadius: '8px', transform: 'scaleX(-1)' }}
             />
-    
-    <Modal isOpen={isOpen} onClose={onClose}>
-      <ModalOverlay />
-      <ModalContent>
-        <ModalHeader>Token</ModalHeader>
-        <ModalCloseButton />
-        <ModalBody>
-          token-12345
-        </ModalBody>
-
-        <ModalFooter>
-        <Button onClick={onCopy}>{hasCopied ? "Copied!" : "Copy"}</Button>
-        </ModalFooter>
-      </ModalContent>
-    </Modal>
-            <Button 
-                onClick={onOpen} 
-                colorScheme="blue" 
-                size="lg">
+            {capturedImage && (
+              <img
+                src={capturedImage}
+                alt="Captured"
+                style={{
+                  position: 'absolute',
+                  top: 0,
+                  left: 0,
+                  width: '100%',
+                  height: '100%',
+                  borderRadius: '8px',
+                  transform: 'scaleX(-1)',
+                }}
+              />
+            )}
+          </Box>
+          <Box mb={4}>
+            <Select
+              placeholder="Select Camera"
+              value={selectedCamera}
+              onChange={handleCameraChange}
+              width={600}
+              color={"black"}
+              isDisabled={isCaptureDisabled}
+            >
+              {cameras.map((camera) => (
+                <option key={camera.deviceId} value={camera.deviceId}>
+                  {camera.label || `Camera ${camera.deviceId.slice(0, 5)}`}
+                </option>
+              ))}
+            </Select>
+          </Box>
+          <Box
+            display='flex'
+            flexDir='row'
+            alignItems='center'
+            justifyContent='center'
+            width={600}
+            mb={4}
+          >
+            {capturedImage ? (
+              <Button
+                colorScheme="red"
+                onClick={() => {
+                  setCapturedImage(null);
+                  setCaptureDisabled(false);
+                  setRegisterDisabled(true);
+                }}
+                width={600}
+                mr={4}
+              >
+                Cancel
+              </Button>
+            ) : null}
+            <Button
+              colorScheme="teal"
+              onClick={captureImage}
+              width={600}
+              isDisabled={isCaptureDisabled}
+            >
+              Capture
+            </Button>
+          </Box>
+          <Box mb={4}>
+            <Button
+              colorScheme="blue"
+              onClick={onOpen}
+              width={600}
+              isDisabled={isRegisterDisabled}
+            >
               Register Exam
             </Button>
-          </VStack>
-        </Center>
-      </ChakraProvider>
-    );
-  }
+          </Box>
+        </Box>
+      </Box>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Token</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            token-12345
+          </ModalBody>
+          <ModalFooter>
+            <Button onClick={onCopy}>{hasCopied ? "Copied!" : "Copy"}</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
 
 export default NewExamPage;
