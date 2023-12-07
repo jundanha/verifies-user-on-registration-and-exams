af3caa030ae5300c02a93d449bddb326df92af43
Jundan Haris <jundanha@gmail.com>
2023-12-07 11:56:25 +0700

    Refactor start exam page layout and form validation

--- client/src/pages/startexam.jsx
+++ client/src/pages/startexam.jsx
@@ -5,61 +5,136 @@ import {
   Button,
   Input,
   Box,
-  ChakraProvider,
-  Center
+  Heading,
+  FormHelperText
 } from '@chakra-ui/react';
 import { Field, Form, Formik } from 'formik';
+import { Link as RouterLink } from 'react-router-dom';
 
 function StartExamPage() {
-  function validateToken(value) {
+  async function handleSubmit(values, actions) {
+    try {
+      console.log(values.token)
+      //  TODO : change the url to the deployed backend
+      const response = await fetch('http://localhost:5000/start_exam', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ token: values.token }),
+      });
+      
+      const responseData = await response.json();
+      console.log(responseData)
+
+      if (response.ok) {
+        window.location.href = '/exam';
+      } else {
+        actions.setFieldError('token', responseData.error || "Token doesn't exist");
+      }
+    } catch (error) {
+      console.error('Error while making API call:', error);
+      actions.setFieldError('token', error.message);
+    }
+  
+    actions.setSubmitting(false);
+  }
+
+  function validateInput(value) {
     let error;
     if (!value) {
       error = 'Token is required';
-    } else if (value.toLowerCase() !== '1234') {
-      error = 'Wrong Token';
     }
     return error;
   }
+  
 
   return (
-    <ChakraProvider>
-      <Box position="absolute" top="10px" left="10px">
-        <Button colorScheme="blue">Back</Button>
-      </Box>
-      <Center h="100vh">
-        <Formik
-          initialValues={{ token: 'Token Here' }}
-          onSubmit={(values, actions) => {
-            setTimeout(() => {
-              alert(JSON.stringify(values, null, 2));
-              actions.setSubmitting(false);
-            }, 1000);
-          }}
+    <>
+      <Box
+      bgGradient="linear(to-tr, #B6FFFA, #687EFF)"
+      w="100%" h="100vh"
+      p={3}
+      color="white"
+      display="flex"
+      alignItems="center"
+      flexDir="column"
+    >
+      <Box
+        display='flex'
+        flexDir='row'
+        alignItems={"center"}
+        justifyContent='space-between'
+        w='100%'
+        px='5'
+      >
+        <Button
+          variant='outline'
+          as={RouterLink}
+          to="/"
         >
-          {(props) => (
-            <Form>
-              <Field name="token" validate={validateToken}>
-                {({ field, form }) => (
-                  <FormControl isInvalid={form.errors.name && form.touched.name}>
-                    <FormLabel>Input your exam token</FormLabel>
-                    <Input {...field} placeholder="Token Here" />
-                    <FormErrorMessage>{form.errors.name}</FormErrorMessage>
-                  </FormControl>
-                )}
-              </Field>
-              <Button
-                mt={4}
-                colorScheme="teal"
-                isLoading={props.isSubmitting}
-                type="submit"
-              >
-                Start Exam
-              </Button>
-            </Form>
-          )}
-        </Formik>
-      </Center>
-    </ChakraProvider>
+          Back
+        </Button>
+        <Heading
+          my={4}
+          fontSize="4xl"
+          color="white"
+          fontFamily={"sans-serif"}
+        >
+          Start Exam
+        </Heading>
+      </Box>
+        <Box
+          display='flex'
+          flexDir='column'
+          alignItems={"center"}
+          justifyContent={"center"}
+          w='100%' h={"100%"}
+        >
+          <Formik
+            initialValues={{ token: '' }}
+            onSubmit={handleSubmit}
+          >
+            {(props) => (
+              <Form>
+                <Field name="token" validate={validateInput}>
+                  {({ field, form }) => (
+                    <FormControl
+                      isInvalid={form.errors.token && form.touched.token}
+                    >
+                      <FormLabel htmlFor="token"
+                        fontSize={"xl"}
+                      >Token</FormLabel>
+                      <FormHelperText
+                        mb='4'
+                      >Enter the token that you get earlier</FormHelperText>
+                      <Input 
+                        {...field} 
+                        id="token" 
+                        placeholder="token" 
+                        backgroundColor={"white"}
+                        color={"black"}
+                      />
+                      <FormErrorMessage>{form.errors.token}</FormErrorMessage>
+                    </FormControl>
+                  )}
+                </Field>
+                <Button
+                  mt={4}
+                  colorScheme="blue"
+                  // eslint-disable-next-line react/prop-types
+                  isLoading={props.isSubmitting}
+                  type="submit"
+                  width={"100%"}
+                >
+                  Start
+                </Button>
+              </Form>
+            )}
+          </Formik>
+        </Box>
+      </Box>
+    </>
   );
 }
 
